c3a1785a894409015d4649611a3a5bc2914f1861
Veli-Pekka Nurmi <nurmi.vp@gmail.com>
2020-12-03 13:27:56 +0200

    3: part2

--- 03.js
+++ 03.js
@@ -6,6 +6,25 @@ readData("03")
   .then((result) => console.log("solution 1:", result)) // 148
   .catch((error) => console.log(error));
 
+readData("03")
+  .then((input) => createMatrix(input))
+  .then((matrix) => {
+    return slopeScenarios.reduce((cumulator, coordinates) => {
+      let treeHits = countTreeHits(matrix, coordinates[0], coordinates[1]);
+      return cumulator === 0 ? treeHits : treeHits * cumulator;
+    }, 0);
+  })
+  .then((result) => console.log("solution 2:", result)) // 727923200
+  .catch((error) => console.log(error));
+
+const slopeScenarios = [
+  [1, 1],
+  [3, 1],
+  [5, 1],
+  [7, 1],
+  [1, 2],
+];
+
 const createMatrix = (data) => {
   const matrix = [];
   for (let i = 0; i < data.length; i++) {
@@ -18,7 +37,7 @@ const countTreeHits = (matrix, moveDown, moveRight) => {
   let count = 0;
   let matrixWidth = matrix[0].length;
   let x = 0;
-  for (let y = 0; y < matrix.length; y++) {
+  for (let y = 0; y < matrix.length; y += moveRight) {
     if (matrix[y][x] === "#") count++;
     x += moveDown;
     if (x > matrixWidth - 1) x -= matrixWidth;
