96173808d105502a6fe5daf71e8fd8bbb05ac0d8
Veli-Pekka Nurmi <nurmi.vp@gmail.com>
2020-12-09 19:26:31 +0200

    9: part 2

--- 09.js
+++ 09.js
@@ -1,8 +1,13 @@
 const { readData } = require("./utils");
 
 readData("09")
-  .then((input) => findInvalidNumber(input.map((x) => Number.parseInt(x))))
+  .then((input) => findInvalidNumber(input.map((x) => Number.parseInt(x)))) // 375054920
   .then((solution) => console.log("solution 1: ", solution))
+  .catch((error) => console.log(error));
+
+readData("09")
+  .then((input) => findSeries(input.map((x) => Number.parseInt(x))))
+  .then((solution) => console.log("solution 2: ", solution)) // 54142584
   .catch((error) => console.log(error));
 
 const findInvalidNumber = (data) => {
@@ -22,4 +27,21 @@ const isSumOfSom = (n, numbers) => {
     }
   }
   return false;
+};
+
+const findSeries = (data) => {
+  const invalidNumber = findInvalidNumber(data);
+  for (let i = 0; i < data.length; i++) {
+    for (let y = i + 1; y < data.length; y++) {
+      const dataSet = data.slice(i, y);
+      const sum = dataSet.reduce((c, i) => {
+        return c + i;
+      }, 0);
+      if (sum === invalidNumber) {
+        const smallest = dataSet.reduce((c, i) => (c > i ? i : c));
+        const largest = dataSet.reduce((c, i) => (c < i ? i : c));
+        return smallest + largest;
+      }
+    }
+  }
 };
